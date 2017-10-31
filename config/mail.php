fcbda7171510af969a814ea48a319e306d262f39
Pitz Michaël <michaeljpitz@gmail.com>
2017-10-31 14:34:02 +0100

    contact ok!

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'relay.skynet.be'),
 
     /*
     |--------------------------------------------------------------------------
@@ -87,6 +87,8 @@ return [
     'username' => env('MAIL_USERNAME'),
 
     'password' => env('MAIL_PASSWORD'),
+
+    'from' => ['address' => 'michael.pitz@cf2m.be', 'name' => 'Administrateur'],
 
     /*
     |--------------------------------------------------------------------------
