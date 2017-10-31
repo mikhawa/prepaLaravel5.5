73f98f5d7aa10fbcd9809f1fc1dcebef676accfb
Pitz Michaël <michaeljpitz@gmail.com>
2017-10-31 10:21:59 +0100

    Routages et réponses

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('{n}', function($n) {
+    return 'Je suis la page ' . $n . ' !';
+})->where('n', '[1-3]');
+Route::get('/v1', function () {
+    return view('vue1');
+});
+Route::get('article/{n}', function($n) {
+    return view('article')->with('numero', $n);
+})->where('n', '[0-9]+');
+Route::get('facture/{n}', function($n) {
+    return view('facture')->withNumero($n);
+})->where('n', '[0-9]+');
